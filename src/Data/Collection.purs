2c743cb7dc4a17ac5fed712abc582d9ce4bcd253
joneshf <jones3.hardy@gmail.com>
2014-04-09 13:09:59 -0700

    Add Maybe and Tree.

--- src/Data/Collection.purs
+++ src/Data/Collection.purs
@@ -1,6 +1,7 @@
 module Data.Collection where
 
   import Data.Array ((:))
+  import Data.Maybe
   import Data.Monoid
   import Data.Tuple
 
@@ -11,15 +12,29 @@ module Data.Collection where
     show SeqNull = "SeqNull"
     show (SeqCons x xs) = "SeqCons(" ++ show x ++ ", " ++ show xs ++ ")"
 
+  -- Leafy tree
+  data Tree a = Bud
+              | Leaf a
+              | Branch (Tree a) (Tree a)
+
+  instance showTree :: (Show a) => Show (Tree a) where
+    show Bud = "Bud"
+    show (Leaf x) = "Leaf(" ++ show x ++ ")"
+    show (Branch l r) = "Branch(" ++ show l ++ ", " ++ show r ++ ")"
+
   -- | This differs from the standard Foldable
   --   in that it doesn't attempt to impose an ordering.
+  --   specifically, there is no notion of `foldl` or `foldr`
   class Foldable f where
+    -- `fold` is the minimum required to implement a `Foldable`.
     fold :: forall a b. (a -> b -> b) -> b -> f a -> b
+    -- `size` should have a default implementation.
+    size :: forall a. f a -> Number
+    -- size = fold (const ((+) 1)) 0
 
   -- | An unordered collection of things.
   class (Foldable c, Monoid (c a)) <= Collection c a where
-    -- `size` and `cons` are the minimum required to implement a `Collection`
-    size :: c a -> Number
+    -- cons` is the minimum required to implement a `Collection`.
     cons :: a -> c a -> c a
 
     -- `filter` and `partition` should have default implementations.
@@ -36,40 +51,103 @@ module Data.Collection where
     -- first xs = case last (reverse xs) of
     --   SeqCons x xs' -> SeqCons x (reverse xs')
     --   SeqNull -> SeqNull
-    snoc :: s a -> a -> s a
-    -- snoc xs x = reverse (cons x (reverse xs))
     last :: s a -> SeqView s a
     -- last xs = case first (reverse xs) of
     --   SeqCons x xs' -> SeqCons x (reverse xs')
     --   SeqNull -> SeqNull
     reverse :: s a -> s a
     -- reverse xs = foldl (flip cons) mempty xs
+    snoc :: s a -> a -> s a
+    -- snoc xs x = reverse (cons x (reverse xs))
+
+  -- Array
 
   instance foldableArray :: Foldable [] where
     fold _ z [] = z
     fold f z (x:xs) = x `f` (fold f z xs)
 
-  instance collectionArray :: Collection [] a where
-    size [] = 0
-    size (_:xs) = 1 + size xs
+    size = fold (const ((+) 1)) 0
 
+  instance collectionArray :: Collection [] a where
     cons = (:)
 
     filter p xs = fold (\x acc -> if p x then cons x acc else acc) mempty xs
     partition p xs = Tuple (filter p xs) (filter (not <<< p) xs)
 
   instance sequenceArray :: Sequence [] a where
-    snoc xs x = xs ++ [x]
+    foldl _ z [] = z
+    foldl f z (x:xs) = foldl f (z `f` x) xs
 
     first [] = SeqNull
     first (x:xs) = SeqCons x xs
 
-    last [] = SeqNull
-    last [x] = SeqCons x []
-    last (x:xs) = case last xs of
-      SeqCons x' xs' -> SeqCons x' (x:xs')
+    last xs = case first (reverse xs) of
+      SeqCons x xs' -> SeqCons x (reverse xs')
+      SeqNull -> SeqNull
 
-    foldl _ z [] = z
-    foldl f z (x:xs) = foldl f (z `f` x) xs
+    snoc xs x = xs ++ [x]
 
     reverse = Data.Array.reverse
+
+  -- Maybe
+
+  instance foldableMaybe :: Foldable Maybe where
+    fold _ z Nothing = z
+    fold f z (Just x) = x `f` z
+
+    size = fold (const ((+) 1)) 0
+
+  instance semigroupMaybe :: (Semigroup a) => Semigroup (Maybe a) where
+    (<>) (Just x) (Just y) = Just (x <> y)
+    (<>) _        _        = Nothing
+
+  instance monoidMaybe :: (Monoid a) => Monoid (Maybe a) where
+    mempty = Nothing
+
+  instance collectionMaybe :: (Monoid a) => Collection Maybe a where
+    cons x Nothing = Just x
+    cons x (Just y) = Just (x <> y)
+
+    filter p xs = fold (\x acc -> if p x then cons x acc else acc) mempty xs
+    partition p xs = Tuple (filter p xs) (filter (not <<< p) xs)
+
+  instance sequenceMaybe :: (Monoid a) => Sequence Maybe a where
+    foldl _ z Nothing = z
+    foldl f z (Just x) = z `f` x
+
+    first Nothing = SeqNull
+    first (Just x) = SeqCons x Nothing
+
+    last x = first x
+
+    snoc Nothing x = Just x
+    snoc (Just x) y = Just (x <> y)
+
+    reverse = id
+
+  -- Tree
+
+  instance foldableTree :: Foldable Tree where
+    fold _ z _  = z
+    fold f z (Leaf x) = x `f` z
+    fold f z (Branch l r) = fold f (fold f z l) r
+
+    size = fold (const ((+) 1)) 0
+
+  -- Using a `Tree` as a `Semigroup` only makes sense if we ignore
+  -- the structure of the tree, and focus on the semantics of a tree.
+  -- Semantic equality is necessary for the laws to hold.
+  -- This will work fine for `Collection`, but probably not so much for `Sequence`.
+  instance semigroupTree :: Semigroup (Tree a) where
+    (<>) Bud t   = t
+    (<>) t   Bud = t
+    (<>) l   r   = Branch l r
+
+  instance monoidTree :: Monoid (Tree a) where
+    mempty = Bud
+
+  instance collectionTree :: Collection Tree a where
+    cons x t = Leaf x <> t
+
+    filter p xs = fold (\x acc -> if p x then cons x acc else acc) mempty xs
+    partition p xs = Tuple (filter p xs) (filter (not <<< p) xs)
